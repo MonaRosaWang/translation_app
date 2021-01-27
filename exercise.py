2c0b51a91ac211c9f8eb57e683ea4c485d8c0e56
rosa <rosawang77@gmail.com>
2021-01-26 17:25:07 -0800

    First commit

--- exercise.py
+++
@@ -1,9 +0,0 @@
-import json
-from difflib import get_close_matches
-data = json.load(open('data.json'))
-
-word = input ('Enter word: ')
-    if word in data:
-        return data[word]
-    else:
-        return 'none'
